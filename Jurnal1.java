8e9ba4e4dc496f980ad5663f6073dc26bcc26c36
firmanali2 <somelshop@yahoo.co.id>
2014-04-24 17:53:42 +0700

    Update Jurnal1.java

--- Jurnal1.java
+++ Jurnal1.java
@@ -5,13 +5,16 @@ public class Jurnal1 {
     public static void main(String[] args) {
         // TODO code application logic here
         int n,x;
+        String str1=new String();
+        String str2=new String();
         Fibonacci f=new Fibonacci();
         Scanner sc = new Scanner(System.in);
         System.out.print("Input jumlah digit: ");
         n=sc.nextInt();
         for (int i=1; i<=n;i++){
-            //x=f.hitungFibonacci(n);
-            System.out.print(f.hitungFibonacci(i)+" ");
+            str1=Integer.toString(f.hitungFibonacci(i));
+            str2=str1+" "+str2;
         }
+        System.out.println(str2);
     }
 }
